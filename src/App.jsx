1d991164b2b5bca5dc90d9cce9adcf5e94016e4f
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-01-30 20:14:49 +0600

    Header, Movielist, Sidebar, Footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import MovieList from "./cine/MovieList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-center text-blue-700">Hello</h1>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
